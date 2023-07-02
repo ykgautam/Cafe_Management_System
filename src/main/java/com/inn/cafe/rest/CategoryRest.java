672230ada2101bfd0431386a25851da4253115aa
Yash <yk.gautam97@gmail.com>
2023-07-02 14:52:44 +0530

    created Add, Get and Update API of Category

---
+++ src/main/java/com/inn/cafe/rest/CategoryRest.java
@@ -0,0 +1,21 @@
+package com.inn.cafe.rest;
+
+import com.inn.cafe.POJO.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping(path = "/category")
+public interface CategoryRest {
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<String> addNewCategory(@RequestBody(required = true) Map<String, String> requestMap);
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<List<Category>> getAllCategory(@RequestParam(required = false) String filterValue);
+
+    @PostMapping(path = "/update")
+    public ResponseEntity<String> updateCategory(@RequestBody(required = true) Map<String, String> requestMap);
+}
